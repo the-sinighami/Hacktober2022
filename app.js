44dd1d7a42a10b5494f52fb601fda4831ff83762
Alok Rawat <98082696+tonystark2121@users.noreply.github.com>
2022-10-22 16:56:53 +0530

    Portfolio App using reactjs

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
 
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true);
+    }
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onClick={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+        })}
+      </section>
+    </>
+  )
+}
+
+export default App
